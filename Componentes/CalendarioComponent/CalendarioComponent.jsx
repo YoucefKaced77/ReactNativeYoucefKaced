f5b33b5f300f3a4ef6250a7cbfdc5b39cfa0722f
youcef <kaced.127157@e.unavarra.es>
2023-04-04 20:06:18 +0200

    Stack Navigation

--- Componentes/CalendarioComponent/CalendarioComponent.jsx
+++ Componentes/CalendarioComponent/CalendarioComponent.jsx
@@ -1,28 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { ListItem, Avatar } from '@rneui/themed';
 import { SafeAreaView, FlatList } from 'react-native';
+import { EXCURSIONES } from "../../Comun/excursiones"
 
-function Calendario(props) {
+class Calendario extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            excursiones: EXCURSIONES
+        };
+    }
+
+    render(){
+
+    const { navigate } = this.props.navigation;    
 
     const renderCalendarioItem = ({item, index}) => {
         return (
-            <ListItem key={index} bottomDivider onPress={() => props.onPress(item.id)}>
+            <ListItem
+            key={index}
+            onPress={() => navigate('DetalleExcursion', { excursionId: item.id })}
+            bottomDivider>
                 <Avatar source={require('../imagenes/40Años.png')} />
                 <ListItem.Content>
                     <ListItem.Title>{item.nombre}</ListItem.Title>
                     <ListItem.Subtitle>{item.descripcion}</ListItem.Subtitle>
                 </ListItem.Content>
-            </ListItem>
+            </ListItem> 
         );
     };
+
     return (
         <SafeAreaView>
-        <FlatList
-            data={props.excursiones}  
-            renderItem={renderCalendarioItem}
-            keyExtractor={item => item.id.toString()}
-        />
+            <FlatList 
+                data={this.state.excursiones}
+                renderItem={renderCalendarioItem}
+                keyExtractor={item => item.id.toString()}
+            />
         </SafeAreaView>
     );
+    }
 }
+
 export default Calendario;
